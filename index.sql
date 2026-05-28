5fc1233ed6f7ce2c99563cb7a56f45e8673e61d7
Melkzedk <melkywafula29@gmail.com>
2026-05-28 14:20:18 +0300

    changes

--- index.sql
+++ index.sql
@@ -138,15 +138,3 @@ SELECT @@VERSION;
 
 /*Schemas */
 
-CREATE DATABASE CompanyDB;
-
-CREATE SCHEMA HR;
-
-CREATE SCHEMA Sales;
-
-CREATE TABLE HR.Employees (
-    EmployeeID INT PRIMARY KEY,
-    FullName VARCHAR(100),
-    Department VARCHAR(50),
-    Salary DECIMAL(10,2)
-);
