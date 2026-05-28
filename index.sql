b8d23c86ec64882087bee135aea24f173aed055f
Melkzedk <melkywafula29@gmail.com>
2026-05-28 08:08:30 +0300

    small changes

--- index.sql
+++ index.sql
@@ -144,4 +144,9 @@ CREATE SCHEMA HR;
 
 CREATE SCHEMA Sales;
 
-
+CREATE TABLE HR.Employees (
+    EmployeeID INT PRIMARY KEY,
+    FullName VARCHAR(100),
+    Department VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
