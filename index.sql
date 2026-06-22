d759d2f489a4a9550465300d825daad634d112bc
Melkzedek <Melkwaf@DESKTOP-4Q1B269.LIAISONX.liaisongroup.net>
2026-06-22 08:56:10 +0300

    small changes

--- index.sql
+++ index.sql
@@ -140,13 +140,3 @@ SELECT @@VERSION;
 
 CREATE DATABASE CompanyDB;
 
-CREATE SCHEMA HR;
-
-CREATE SCHEMA Sales;
-
-CREATE TABLE HR.Employees (
-    EmployeeID INT PRIMARY KEY,
-    FullName VARCHAR(100),
-    Department VARCHAR(50),
-    Salary DECIMAL(10,2)
-);
